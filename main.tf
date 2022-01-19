a8016223ba1b476b830a7036558c83c95d259b8e
prasadanjay <prasad.anjay@gmail.com>
2022-01-19 18:24:08 +0530

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+//Normal EC2 instance
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+
+  tags = {
+	Name = "Test-Example"
+ }
+}
